df12433991466ec10db7bad36898afc3ecae5cec
HHB <bcl0206@gmail.com>
2023-08-29 08:20:56 +0900

    style:signup 페이지 UI 구성

--- src/pages/SignupPage/index.tsx
+++ src/pages/SignupPage/index.tsx
@@ -1,5 +1,21 @@
+import * as Styled from '../SigninPage/styled';
+
 const SignupPage = () => {
-  return <div>upPage</div>;
+  const handleSubmit = () => {
+    alert('회원가입');
+  };
+
+  return (
+    <Styled.AuthContainer>
+      <Styled.AuthInner>
+        <Styled.AuthBox onSubmit={handleSubmit}>
+          <Styled.StyledInput type="text" placeholder="아이디" />
+          <Styled.StyledInput type="password" placeholder="비밀번호" />
+          <Styled.StyledButton>회원가입</Styled.StyledButton>
+        </Styled.AuthBox>
+      </Styled.AuthInner>
+    </Styled.AuthContainer>
+  );
 };
 
 export default SignupPage;
