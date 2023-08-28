2aa454fde1c55fd73a33cf806f03272fe083bc76
HHB <bcl0206@gmail.com>
2023-08-29 07:45:08 +0900

    style:signin 페이지 UI 구성

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,5 +1,74 @@
+import { styled } from 'styled-components';
+
 const SigninPage = () => {
-  return <div>SigninPage</div>;
+  const handleSubmit = () => {
+    alert('로그인');
+  };
+
+  return (
+    <AuthContainer>
+      <AuthInner>
+        <AuthBox onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="아이디" />
+          <StyledInput type="password" placeholder="비밀번호" />
+          <StyledButton>로그인</StyledButton>
+        </AuthBox>
+      </AuthInner>
+    </AuthContainer>
+  );
 };
 
 export default SigninPage;
+
+const AuthContainer = styled.main`
+  min-height: 100vh;
+  margin-top: 50px;
+`;
+
+const AuthInner = styled.section`
+  background-color: white;
+  width: 1080px;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AuthBox = styled.form`
+  background-color: #03c75a;
+  width: 500px;
+  height: 500px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.42);
+`;
+
+const StyledInput = styled.input`
+  width: 400px;
+  height: 50px;
+  border: none;
+  border-radius: 5px;
+  outline: none;
+  padding-left: 10px;
+  font-size: 20px;
+  box-sizing: border-box;
+  box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.42);
+`;
+
+const StyledButton = styled.button`
+  background-color: #e9e9e9;
+  border: none;
+  border-radius: 5px;
+  width: 200px;
+  height: 50px;
+  font-size: 15px;
+  box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.42);
+  margin-top: 20px;
+  cursor: pointer;
+`;
