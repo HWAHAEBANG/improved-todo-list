f846432c716d62dceb2c1700522cbce38f0c485b
HHB <bcl0206@gmail.com>
2023-08-28 21:16:48 +0900

    feat: header컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Header from './components/common/Header';
 
 function App() {
   return (
     <div className="App">
-      app
+      <Header />
       <Outlet />
     </div>
   );
