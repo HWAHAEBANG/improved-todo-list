d171861f5558ca34032e2b819222c67d3ca93752
HHB <bcl0206@gmail.com>
2023-08-29 10:06:24 +0900

    feat: 투두 추가 컴포넌트 기본 로직 구성

--- src/components/todo/TodoInput.tsx
+++ src/components/todo/TodoInput.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+import type { ChangeEventHandler /* , FormEventHandler */ } from 'react';
+
 const TodoInput = () => {
-  return <div>TodoInput</div>;
+  const [intputValue, setInputValue] = useState('');
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
+    setInputValue(e.currentTarget.value);
+  };
+
+  const handleSubmit = () => {
+    alert('추가');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={intputValue}
+        onChange={handleChange}
+        placeholder="할 일을 입력해주세요."
+      />
+      <button>추가</button>
+    </form>
+  );
 };
 
 export default TodoInput;
