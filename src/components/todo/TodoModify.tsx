076013f2f4ba692a273575c93dfd1e58844ce840
HHB <bcl0206@gmail.com>
2023-08-29 10:34:50 +0900

    feat: 투두 수정 컴포넌트 기본 로직 구성

--- src/components/todo/TodoModify.tsx
+++ src/components/todo/TodoModify.tsx
@@ -1,5 +1,34 @@
+import { ChangeEventHandler, useState } from 'react';
+
 const TodoModify = () => {
-  return <div>TodoModify</div>;
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
+    setInputValue(e.currentTarget.value);
+  };
+
+  const handleSubmit = () => {
+    alert('제출');
+  };
+
+  const handleCancel = () => {
+    alert('삭제');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="할 일을 입력해주세요."
+      />
+      <button type="submit">제출</button>
+      <button type="button" onClick={handleCancel}>
+        취소
+      </button>
+    </form>
+  );
 };
 
 export default TodoModify;
