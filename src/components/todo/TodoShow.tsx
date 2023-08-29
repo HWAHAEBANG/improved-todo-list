7294983696a367554ca2072f08b8e132d6d66dad
HHB <bcl0206@gmail.com>
2023-08-29 10:50:23 +0900

    feat: 투두 보기 컴포넌트 기본 로직 구성

--- src/components/todo/TodoShow.tsx
+++ src/components/todo/TodoShow.tsx
@@ -1,5 +1,25 @@
-const TodoShow = () => {
-  return <div>TodoShow</div>;
+import { FC } from 'react';
+
+interface Props {
+  isEditMode: () => {};
+}
+
+const TodoShow: FC<Props> = ({ isEditMode }) => {
+  const handleDelete = () => {
+    alert('삭제');
+  };
+
+  return (
+    <form>
+      <p>투두내용</p>
+      <button type="button" onClick={isEditMode}>
+        수정
+      </button>
+      <button type="button" onClick={handleDelete}>
+        삭제
+      </button>
+    </form>
+  );
 };
 
 export default TodoShow;
