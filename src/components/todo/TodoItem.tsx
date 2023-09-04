c8cd659b9abdc6d1f2187f19ce905720eec9a161
HHB <bcl0206@gmail.com>
2023-09-04 19:38:37 +0900

    feat: 수정된 배열 새로만들기 로직 구현

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import TodoCheckbox from './TodoCheckbox';
 import TodoModify from './TodoModify';
 import TodoShow from './TodoShow';
 import { styled } from 'styled-components';
+import { Todo } from '../../types/todo';
 
-const TodoItem = () => {
+interface Props {
+  todo: Todo;
+  setTodo: (targetId: number, newTodo: Todo) => void;
+}
+
+const TodoItem: FC<Props> = ({ todo, setTodo }) => {
   const [isEditMode, setIsEditMode] = useState(false);
 
   return (
     <TodoItemBox>
-      <TodoCheckbox />
+      <TodoCheckbox todo={todo} setTodo={setTodo} />
       {isEditMode ? (
-        <TodoModify setIsEditMode={setIsEditMode} />
+        <TodoModify setIsEditMode={setIsEditMode} todo={todo} setTodo={setTodo} />
       ) : (
-        <TodoShow setIsEditMode={setIsEditMode} />
+        <TodoShow setIsEditMode={setIsEditMode} todo={todo} />
       )}
     </TodoItemBox>
   );
--- src/components/todo/TodoListSection.tsx
+++ src/components/todo/TodoListSection.tsx
@@ -1,10 +1,26 @@
 import { styled } from 'styled-components';
 import TodoItem from './TodoItem';
+import { todosContext } from '../../contexts/todosContext';
+import { useContext } from 'react';
+import { Todo } from '../../types/todo';
 
 const TodoListSection = () => {
+  const { todos, setTodos } = useContext(todosContext);
+
+  const setTodo = (targetId: number, newTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === targetId) {
+        return newTodo;
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <TodoListWrap>
-      <TodoItem />
+      {todos?.map(todo => <TodoItem key={todo.id} todo={todo} setTodo={setTodo} />)}
     </TodoListWrap>
   );
 };
