c4319793b77fc4f35b187e183e1d18faa4aac655
HHB <bcl0206@gmail.com>
2023-09-04 19:34:49 +0900

    feat: 체크박스 수정 로직 구현

--- src/components/todo/TodoCheckbox.tsx
+++ src/components/todo/TodoCheckbox.tsx
@@ -1,9 +1,24 @@
-const TodoCheckbox = () => {
+import { FC } from 'react';
+import { Todo } from '../../types/todo';
+import { todo as todoApi } from '../../apis/todo';
+
+interface Props {
+  todo: Todo;
+  setTodo: (targetId: number, newTodo: Todo) => void;
+}
+
+const TodoCheckbox: FC<Props> = ({ todo, setTodo }) => {
   const handleCheck = () => {
-    alert('check');
+    todoApi
+      .updateTodo(todo.id, todo.todo, !todo.isCompleted)
+      .then(response => {
+        const updatedData = response.data;
+        setTodo(todo.id, updatedData);
+      })
+      .catch(console.error);
   };
 
-  return <input type="checkbox" /* checked={} */ onChange={handleCheck} />;
+  return <input type="checkbox" checked={todo.isCompleted} onChange={handleCheck} />;
 };
 
 export default TodoCheckbox;
