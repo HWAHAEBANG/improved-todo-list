ff7e3415f22239d57fc8ea4e25dc774a95ee58c3
HHB <bcl0206@gmail.com>
2023-09-04 11:16:21 +0900

    feat: protected route 토큰 확인 로직 추가

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,10 +1,15 @@
 import React, { JSX } from 'react';
+import { getToken } from '../utils/localStorage';
+import { Navigate } from 'react-router-dom';
 
 interface Props {
   children: JSX.Element;
 }
 
 const ProtectedRoute: React.FC<Props> = ({ children }) => {
+
+  if(!getToken()) return <Navigate to='/signin'/>
+
   return children;
 };
 
--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,10 +1,15 @@
 import React, { JSX } from 'react';
+import { getToken } from '../utils/localStorage';
+import { Navigate } from 'react-router-dom';
 
 interface Props {
   children: JSX.Element;
 }
 
 const PublicRoute: React.FC<Props> = ({ children }) => {
+
+  if(getToken()) return <Navigate to='/todo'/>;
+
   return children;
 };
 
